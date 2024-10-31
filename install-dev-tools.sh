e81881c341dc6e7d09314bfc1105d5f41bc7a51a
Fernando Dantas <fernandodantas1996@gmail.com>
2024-10-31 19:08:37 -0300

    feat: adiciona descrições dos mapeamentos do vim

--- install-dev-tools.sh
+++ install-dev-tools.sh
@@ -1,4 +1,4 @@
-sudo apt install git guake alacritty htop make
+sudo apt install git guake alacritty htop make xclip ripgrep
 
 # neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -7,6 +7,7 @@ sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
 
 # .net 8.0
 sudo apt install dotnet-sdk-8.0
+dotnet tool install --global fsautocomplete
 
 # jetbrains nerd font
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip"
@@ -15,3 +16,14 @@ unzip JetBrainsMono.zip
 cd ..
 sudo mv jetbrainsmono /usr/share/fonts
 sudo fc-cache -fv
+
+# jetbrains toolbox
+# pré-requisito para jetbrains toolbox
+sudo apt install libfuse2
+
+wget -cO jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
+tar -xzf jetbrains-toolbox.tar.gz
+DIR=$(find . -maxdepth 1 -type d -name jetbrains-toolbox-\* -print | head -n1)
+mv $DIR/jetbrains-toolbox /usr/local/bin
+rm -rf $DIR
+rm jetbrains-toolbox.tar.gz
--- private_dot_config/nvim/lua/config/mappings.lua
+++ private_dot_config/nvim/lua/config/mappings.lua
@@ -8,25 +8,25 @@ map('n', '<C-j>', '<C-w>j', opts)
 map('n', '<C-k>', '<C-w>k', opts)
 
 -- divisão dos panels
-map('n', '|', '<C-w>v', opts)
-map('n', '_', '<C-w>s', opts)
+map('n', '|', '<C-w>v', { desc = 'Split horizontally', unpack(opts) })
+map('n', '_', '<C-w>s', { desc = 'Split vertically', unpack(opts) })
 
 -- esc como jj
-map('i', 'jj', '<esc>', opts)
+map('i', 'jj', '<esc>', { desc = 'Easy exit', unpack(opts) })
 
 -- nvim-tree
-map('n', '<Leader>e', ':NvimTreeToggle<CR>', opts)
+map('n', '<Leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle tree', unpack(opts) })
 
 -- telescope
-map('n', '<Leader>ff', ':Telescope find_files<CR>', opts)
-map('n', '<Leader>fh', ':Telescope command_history<CR>', opts)
-map('n', '<Leader>/', ':Telescope live_grep<CR>', opts)
-map('n', '<Leader>fb', ':Telescope buffers<CR>', opts)
+map('n', '<Leader>ff', ':Telescope find_files<CR>', { desc = 'Find files', unpack(opts) })
+map('n', '<Leader>fh', ':Telescope command_history<CR>', { desc = 'Search command history', unpack(opts) })
+map('n', '<Leader>/', ':Telescope live_grep<CR>', { desc = 'GREP!', unpack(opts) })
+map('n', '<Leader>fb', ':Telescope buffers<CR>', { desc = 'Find buffers', unpack(opts) })
 
 -- lsp
-map('n', '<Leader>lf', ':lua vim.lsp.buf.format()<CR>', opts)
+map('n', '<Leader>lf', ':lua vim.lsp.buf.format()<CR>', { desc = 'Format file', unpack(opts) })
 
 -- tabs
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
-map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', { desc = 'Prev buffer', unpack(opts) })
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', { desc = 'Next buffer', unpack(opts) })
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', { desc = 'Pick buffer', unpack(opts) } )
