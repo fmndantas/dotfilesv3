6f4507ae59d00916a0f37c3756eaad066f456ce2
Fernando Dantas <fernandodantas1996@gmail.com>
2024-10-31 19:28:34 -0300

    feat: lazygit

--- install-dev-tools.sh
+++ install-dev-tools.sh
@@ -27,3 +27,9 @@ DIR=$(find . -maxdepth 1 -type d -name jetbrains-toolbox-\* -print | head -n1)
 mv $DIR/jetbrains-toolbox /usr/local/bin
 rm -rf $DIR
 rm jetbrains-toolbox.tar.gz
+
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
