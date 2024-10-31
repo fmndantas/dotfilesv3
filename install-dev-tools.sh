b3422310b30550c6c6444de5f95be72c1b8b32fc
Fernando Dantas <fernandodantas1996@gmail.com>
2024-10-31 16:44:34 -0300

    feat: seta alacritty como terminal padrão do i3

--- install-dev-tools.sh
+++ install-dev-tools.sh
@@ -1,6 +1,6 @@
 sudo apt install git guake alacritty htop make
 
-# neovimrh
+# neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
