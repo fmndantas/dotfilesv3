3097435946cbc5883701e3688feb04e5cddd4c11
Fernando <fernandodantas1996@gmail.com>
2024-11-19 08:44:42 -0300

    delete: remove scripts desnecessários

--- install-i3.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install i3 i3lock polybar arandr flameshot feh picom
-
-# NOTE: para brilho do polybar funcionar
-# WARN: desabilitado por enquanto
-# sudo addgroup video
-# sudo usermod -aG video $(whoami)
