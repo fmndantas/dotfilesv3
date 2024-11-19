6cb8f9ddad0062a627e27f0be666db7bc262e41b
Fernando <fernandodantas1996@gmail.com>
2024-11-19 08:44:51 -0300

    fix: ajusta install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,6 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
 
 # NOTE: python3
 sudo apt install python3.12-venv
