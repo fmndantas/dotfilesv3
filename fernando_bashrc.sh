ec1b68865f23a4f6d0509beb5474931e13e094ee
Fernando <fernandodantas1996@gmail.com>
2024-12-10 12:25:33 -0300

    feat: .bashrc

---
+++ fernando_bashrc.sh
@@ -0,0 +1,11 @@
+export DOTNET_ROOT="/home/fernando/.dotnet"
+export EDITOR="nvim"
+
+alias dt='dotnet test'
+alias dtw='dotnet watch test'
+alias dtf='dotnet test --filter'
+alias dtwf='dotnet watch test --filter'
+alias dr='dotnet run'
+alias dcdb='dotnet clean && dotnet build'
+
+alias ..='cd ..'
