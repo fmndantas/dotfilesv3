42b48742344e869a19461e7cf8e2d33b52708155
Fernando <fernandodantas1996@gmail.com>
2024-12-20 08:57:16 -0300

    neovim/fix: defaults on but without C-h

--- private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,10 +5,8 @@ return
   dependencies = {
     -- main one
     { "ms-jpq/coq_nvim", branch = "coq" },
-
     -- 9000+ Snippets
     { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
     -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
     -- Need to **configure separately**
     { 'ms-jpq/coq.thirdparty', branch = "3p" }
@@ -22,9 +20,8 @@ return
     vim.g.coq_settings = {
         auto_start = true, 
         keymap = {
-          recommended = false,
-          jump_to_mark = '',
-          pre_select = true,
+          recommended = true,
+          jump_to_mark = 'null'
         },
         auto_start = 'shut-up',
         display = {
@@ -33,7 +30,7 @@ return
           }
         },
         completion = {
-          always = false,
+          always = true,
         }
       }
   end,
